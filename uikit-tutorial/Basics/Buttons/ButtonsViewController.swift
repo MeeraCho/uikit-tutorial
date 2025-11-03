4c4740eb4e70ee6fad1766eaa4c9e5b4d4facdce
MiraCho <miracho119@gmail.com>
2025-11-03 14:50:48 -0700

    added button lable

--- uikit-tutorial/Basics/Buttons/ButtonsViewController.swift
+++ uikit-tutorial/Basics/Buttons/ButtonsViewController.swift
@@ -15,11 +15,22 @@ class ButtonsViewController: UIViewController {
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitleColor(.white, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(onShowNameTapped), for: .touchUpInside)
         return button
+    }()
+    
+    private var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "How are you?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isHidden = false
+        return label
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        showNameButton.addTarget(self, action: #selector(onShowNameTapped), for: .touchUpInside)
 
         view.addSubview(showNameButton)
         showNameButton.widthAnchor.constraint(equalToConstant: 350).isActive = true
@@ -28,6 +39,14 @@ class ButtonsViewController: UIViewController {
         showNameButton.layer.cornerRadius = 10
         showNameButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         showNameButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        view.addSubview(nameLabel)
+        nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: showNameButton.bottomAnchor, constant: 16).isActive = true
+    }
+    
+    @objc func onShowNameTapped() {
+        nameLabel.isHidden.toggle()
     }
  
 }
