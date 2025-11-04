a1a1209ac1be2152d4079a5e27660339b3519ce5
MiraCho <miracho119@gmail.com>
2025-11-04 12:27:40 -0700

    added onChange functionality using UIKit

--- uikit-tutorial/Basics/TextFields/TextFieldsViewController.swift
+++ uikit-tutorial/Basics/TextFields/TextFieldsViewController.swift
@@ -22,11 +22,35 @@ class TextFieldsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        textField.delegate = self
+        textField.addTarget(self, action: #selector(onEditingChanged), for: .editingChanged)
+        
         view.addSubview(textField)
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         textField.widthAnchor.constraint(equalToConstant: view.frame.width - 64).isActive = true
+    }
+    
+    @objc func onEditingChanged(_ sender: UITextField){
+        print("DEBUG: Text field text is \(sender.text)")
+    }
+}
+
+extension TextFieldsViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("DEBUG: Return key tapped..")
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("DEBUG: Did begin editing..")
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("DEBUG: Did end editing..")
     }
 }
 
